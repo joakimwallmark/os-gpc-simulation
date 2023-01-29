364fc4a41d00aac82895cf384444d220cf327d84
Joakim Wallmark <wallmark.joakim@gmail.com>
2023-01-29 11:37:13 +0100

    code formatting using lintr and styler

--- 0-scenario-setup.R
+++ 0-scenario-setup.R
@@ -1,7 +1,7 @@
 library(patchwork)
 library(mirt)
 library(TestGardener)
-library(sn) 
+library(sn)
 source("functions/tg_density.R")
 source("functions/arc_length.R")
 source("functions/get_abs_distance.R")
@@ -17,8 +17,8 @@ data_string <- "natmat18"
 test_lengths <- c(14, 28)
 props_os_items <- c(1, 0.5, 0)
 nbins <- 10 # Match original model
-nbasis <- 6 
-spline_order <- 4 
+nbasis <- 6
+spline_order <- 4
 # sample test takers ------------------------------------------------------
 set.seed(12335)
 test_takers <- list()
@@ -42,8 +42,6 @@ test_takers[[1]] <- sort(rnorm(1000))
 test_takers[[2]] <- sort(rsn(1000, xi, omega, alpha))
 test_takers[[3]] <- sort(rnorm(5000))
 test_takers[[4]] <- sort(rsn(5000, xi, omega, alpha))
-# test_takers[[5]] <- sort(rnorm(10000))
-# test_takers[[6]] <- sort(rsn(10000, xi, omega, alpha))
 test_takers_pop <- c("normal", "skewed", "normal", "skewed", "normal", "skewed")
 
 # Load models -------------------------------------------------------------
@@ -57,13 +55,13 @@ data_dens <- tg_theta_density(c(os_mod$parList[[10]]$theta), alpha = 1.5)
 data_dens$plot
 
 # SETUP SCENARIOS ---------------------------------------------------------
-items <- 1:length(response_cats)
-resp_cats <- sort(as.numeric(unique(names(table(response_cats)))), decreasing = T)
+items <- seq_along(response_cats)
+resp_cats <- sort(as.numeric(unique(names(table(response_cats)))), decreasing = TRUE)
 scenarios <- list() # every unique scenario is stored in this list
 scenario_ind <- 1
 set.seed(2422)
-for(length in test_lengths) {
-  for(prop_os_items in props_os_items) {
+for (length in test_lengths) {
+  for (prop_os_items in props_os_items) {
     # sample items
     if (prop_os_items == 1) {
       item_sample <- sample(response_cats, length)
@@ -75,11 +73,11 @@ for(length in test_lengths) {
       mirt_items <- which(names(response_cats) %in% names(item_sample))
     } else {
       item_sample <- sample(response_cats, length)
-      os_sample <- sample(1:length(item_sample), prop_os_items*length)
+      os_sample <- sample(seq_along(item_sample), prop_os_items * length)
       os_items <- which(names(response_cats) %in% names(item_sample[os_sample]))
       mirt_items <- which(names(response_cats) %in% names(item_sample[-os_sample]))
     }
-    for(sample in 1:length(test_takers)) {
+    for (sample in seq_along(test_takers)) {
       mirt_samp <- test_takers[[sample]]
       n <- length(mirt_samp)
       # convert to corresponding tg thetas
@@ -89,41 +87,97 @@ for(length in test_lengths) {
       true_ability_list <- list()
       if (is.null(mirt_items)) {
         total_cat <- sum(response_cats[c(os_items)])
-        true_ability_list$surp_al <- arc_length_surp(os_mod, sample_tg, total_cat, max_surp, item_ids = os_items, mesh_accuracy = 10001)[, 1]
+        true_ability_list$surp_al <-
+          arc_length_surp(
+            os_mod,
+            sample_tg,
+            total_cat,
+            max_surp,
+            item_ids = os_items,
+            mesh_accuracy = 10001
+          )[, 1]
       } else if (is.null(os_items)) {
         total_cat <- sum(response_cats[c(mirt_items)])
-        true_ability_list$surp_al <- arc_length_surp(mirt_mod, mirt_samp, total_cat, max_surp, item_ids = mirt_items, mesh_accuracy = 10001)[, 1]
+        true_ability_list$surp_al <-
+          arc_length_surp(
+            mirt_mod,
+            mirt_samp,
+            total_cat,
+            max_surp,
+            item_ids = mirt_items,
+            mesh_accuracy = 10001
+          )[, 1]
       } else {
-        true_ability_list$surp_al <- arc_length_surp_mixed(mirt_mod, mirt_samp, mirt_items, os_mod, sample_tg, os_items, total_cat, 100, mesh_accuracy = 101)[, 1]
+        true_ability_list$surp_al <-
+          arc_length_surp_mixed(
+            mirt_mod,
+            mirt_samp,
+            mirt_items,
+            os_mod,
+            sample_tg,
+            os_items,
+            total_cat,
+            100,
+            mesh_accuracy = 101
+          )[, 1]
       }
-      
-      surp_rand_mirt <- get_dist_travelled_mirt(mirt_mod, mirt_items, sort(c(mirt_samp, seq(min(mirt_samp), max(mirt_samp), length.out = 10000))))
-      surp_rand_tg <- get_dist_travelled_tg(os_mod, os_items, sort(c(sample_tg, seq(0, 100, length.out = 10000))))
-      
-      prob_rand_mirt <- get_dist_travelled_mirt(mirt_mod, mirt_items, sort(c(mirt_samp, seq(min(mirt_samp), max(mirt_samp), length.out = 10000))), mode = "prob")
-      prob_rand_tg <- get_dist_travelled_tg(os_mod, os_items, sort(c(sample_tg, seq(0, 100, length.out = 10000))), mode = "prob")
-      
-      entr_rand_mirt <- get_dist_travelled_mirt(mirt_mod, mirt_items, sort(c(mirt_samp, seq(min(mirt_samp), max(mirt_samp), length.out = 10000))), mode = "entropy")
-      entr_rand_tg <- get_dist_travelled_tg(os_mod, os_items, sort(c(sample_tg, seq(0, 100, length.out = 10000))), mode = "entropy")
+
+      surp_rand_mirt <-
+        get_dist_travelled_mirt(mirt_mod, mirt_items, sort(c(
+          mirt_samp, seq(min(mirt_samp), max(mirt_samp), length.out = 10000)
+        )))
+      surp_rand_tg <-
+        get_dist_travelled_tg(os_mod, os_items, sort(c(sample_tg, seq(0, 100, length.out = 10000))))
+
+      prob_rand_mirt <-
+        get_dist_travelled_mirt(mirt_mod, mirt_items, sort(c(
+          mirt_samp, seq(min(mirt_samp), max(mirt_samp), length.out = 10000)
+        )), mode = "prob")
+      prob_rand_tg <-
+        get_dist_travelled_tg(os_mod, os_items, sort(c(sample_tg, seq(0, 100, length.out = 10000))), mode = "prob")
+
+      entr_rand_mirt <-
+        get_dist_travelled_mirt(mirt_mod, mirt_items, sort(c(
+          mirt_samp, seq(min(mirt_samp), max(mirt_samp), length.out = 10000)
+        )), mode = "entropy")
+      entr_rand_tg <-
+        get_dist_travelled_tg(os_mod, os_items, sort(c(sample_tg, seq(0, 100, length.out = 10000))), mode = "entropy")
       # pick out the thetas for the sample. Add same person thetas from both models
-      true_ability_list$surp_dist <- surp_rand_tg[match(sample_tg, surp_rand_tg[, 1]), 2] + surp_rand_mirt[match(mirt_samp, surp_rand_mirt[, 1]), 2]
-      true_ability_list$prob_dist <- prob_rand_tg[match(sample_tg, prob_rand_tg[, 1]), 2] + prob_rand_mirt[match(mirt_samp, prob_rand_mirt[, 1]), 2]
-      true_ability_list$entr_dist <- entr_rand_tg[match(sample_tg, entr_rand_tg[, 1]), 2] + entr_rand_mirt[match(mirt_samp, entr_rand_mirt[, 1]), 2]
-      
-      
-      scnario_name <- paste(data_string, "-n", n, "-i", length, "-os-prop", round(prop_os_items, 2), "-", test_takers_pop[sample], sep = "")
-      scenarios[[scenario_ind]] <- list(name = scnario_name,
-                                        pop = test_takers_pop[sample],
-                                        n = n,
-                                        prop_os = prop_os_items,
-                                        os_items = os_items, 
-                                        mirt_items = mirt_items,
-                                        tg_mod = data_dens$density,
-                                        test_taker_thetas = cbind(mirt = mirt_samp, tg = sample_tg),
-                                        true_ability_list = true_ability_list,
-                                        bins = nbins,
-                                        basis = nbasis,
-                                        spline_order = spline_order)
+      true_ability_list$surp_dist <-
+        surp_rand_tg[match(sample_tg, surp_rand_tg[, 1]), 2] + surp_rand_mirt[match(mirt_samp, surp_rand_mirt[, 1]), 2]
+      true_ability_list$prob_dist <-
+        prob_rand_tg[match(sample_tg, prob_rand_tg[, 1]), 2] + prob_rand_mirt[match(mirt_samp, prob_rand_mirt[, 1]), 2]
+      true_ability_list$entr_dist <-
+        entr_rand_tg[match(sample_tg, entr_rand_tg[, 1]), 2] + entr_rand_mirt[match(mirt_samp, entr_rand_mirt[, 1]), 2]
+
+
+      scnario_name <-
+        paste(
+          data_string,
+          "-n",
+          n,
+          "-i",
+          length,
+          "-os-prop",
+          round(prop_os_items, 2),
+          "-",
+          test_takers_pop[sample],
+          sep = ""
+        )
+      scenarios[[scenario_ind]] <- list(
+        name = scnario_name,
+        pop = test_takers_pop[sample],
+        n = n,
+        prop_os = prop_os_items,
+        os_items = os_items,
+        mirt_items = mirt_items,
+        tg_mod = data_dens$density,
+        test_taker_thetas = cbind(mirt = mirt_samp, tg = sample_tg),
+        true_ability_list = true_ability_list,
+        bins = nbins,
+        basis = nbasis,
+        spline_order = spline_order
+      )
       print(paste("scenario", scenario_ind))
       scenario_ind <- scenario_ind + 1
     }
@@ -132,4 +186,3 @@ for(length in test_lengths) {
 
 save(scenarios, file = paste("simulation-data/sim-", data_string, "-scenarios.RData", sep = ""))
 load(file = paste("simulation-data/sim-", data_string, "-scenarios.RData", sep = ""))
-
--- functions/tg_density.R
+++ functions/tg_density.R
@@ -2,17 +2,20 @@ library(gss)
 # for empirical testgardner theta densities
 tg_theta_density <- function(tg_thetas, alpha = 1.4) {
   n <- length(tg_thetas)
-  theta_dens <- tg_thetas[0 < tg_thetas & tg_thetas < 100]
-  dens <- ssden(~ theta_dens, alpha = alpha, domain = data.frame(theta_dens = c(0, 100)))
-  p_max <- sum(tg_thetas == 100)/n
-  p_min <- sum(tg_thetas == 0)/n
-  
-  df <- data.frame(x = seq(0, 100, by = 0.1), y = (1-p_max-p_min)*sapply(seq(0, 100, by = 0.1), function(x) { dssden(dens, x) } ))
+  dens <- ssden(~theta_dens, alpha = alpha, domain = data.frame(theta_dens = c(0, 100)))
+  p_max <- sum(tg_thetas == 100) / n
+  p_min <- sum(tg_thetas == 0) / n
+
+  df <- data.frame(x = seq(0, 100, by = 0.1), y = (1 - p_max - p_min) * sapply(seq(0, 100, by = 0.1), function(x) {
+    dssden(dens, x)
+  }))
   plot_tg <- ggplot(data.frame(tg_thetas), aes(x = tg_thetas)) +
-    geom_histogram(aes(y = ..density..), colour = "black", fill = "white", 
-                   bins = 14, boundary = 0) +
+    geom_histogram(aes(y = ..density..),
+      colour = "black", fill = "white",
+      bins = 14, boundary = 0
+    ) +
     geom_line(data = df, mapping = aes(x, y)) +
-    geom_point(data = data.frame(x=c(0, 100), y=c(p_min, p_max)), mapping = aes(x, y), size = 3) +
+    geom_point(data = data.frame(x = c(0, 100), y = c(p_min, p_max)), mapping = aes(x, y), size = 3) +
     theme_bw() +
     xlab(expression(theta)) +
     ylab(expression(f(theta))) +
@@ -25,11 +28,11 @@ tg_theta_density <- function(tg_thetas, alpha = 1.4) {
 tg_qss <- function(p, dens_list) {
   q <- vector("numeric", length = length(p))
   q[p < dens_list$p_min] <- 0
-  q[p > (1-dens_list$p_max)] <- 100
-  den_ind <- dens_list$p_min < p & p < (1-dens_list$p_max)
+  q[p > (1 - dens_list$p_max)] <- 100
+  den_ind <- dens_list$p_min < p & p < (1 - dens_list$p_max)
   if (sum(den_ind)) {
     p_dens <- p[den_ind]
-    p_dens <- (p_dens-dens_list$p_min)/(1-dens_list$p_min-dens_list$p_max)
+    p_dens <- (p_dens - dens_list$p_min) / (1 - dens_list$p_min - dens_list$p_max)
     q[den_ind] <- qssden(dens_list$dens_obj, p_dens)
   }
   return(q)
@@ -43,7 +46,7 @@ tg_pss <- function(x, dens_list) {
   p[x == 100] <- 1
   den_ind <- 0 < x & x < 100
   if (sum(den_ind)) {
-    p[den_ind] <- dens_list$p_min + (1-dens_list$p_min-dens_list$p_max)*pssden(dens_list$dens_obj, x[den_ind])
+    p[den_ind] <- dens_list$p_min + (1 - dens_list$p_min - dens_list$p_max) * pssden(dens_list$dens_obj, x[den_ind])
   }
   return(p)
 }
@@ -55,7 +58,7 @@ tg_dss <- function(x, dens_list) {
   d[x == 100] <- dens_list$p_max
   den_ind <- 0 < x & x < 100
   if (sum(den_ind)) {
-    d[den_ind] <- (1 - dens_list$p_min - dens_list$p_max)*dssden(dens_list$dens_obj, x[den_ind])
+    d[den_ind] <- (1 - dens_list$p_min - dens_list$p_max) * dssden(dens_list$dens_obj, x[den_ind])
   }
   return(d)
 }
@@ -65,7 +68,7 @@ tg_rss <- function(n, dens_list) {
   theta <- sample(
     c(0, 1, 100),
     n,
-    replace = T,
+    replace = TRUE,
     probs = c(
       dens_list$p_min,
       1 - dens_list$p_min - dens_list$p_max,
